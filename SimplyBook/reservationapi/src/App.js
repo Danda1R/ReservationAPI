df98ece0a2888c4a552aec041dfd50f22b6ef9f0
Rishik Danda <rishikdanda@Rishiks-MacBook-Pro.local>
2023-07-22 21:15:03 -0400

    Successfully reservation integration

--- SimplyBook/reservationapi/src/App.js
+++ SimplyBook/reservationapi/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { API } from "aws-amplify";
+import { API, graphqlOperation } from "aws-amplify";
 import Button from "./Button";
 
 import * as mutations from "./graphql/mutations.js";
@@ -48,10 +48,6 @@ export default function App() {
       var accessToken = json_text["token"];
       var refreshToken = json_text["refresh_token"];
 
-      var accessToken =
-        "ef268f1916bb69c19b53e241cfbb34e6a82f3c7e68b9ac02f87e5ca2d90357d2";
-      var refreshToken =
-        "5a1bb5c8dbfb12e8dc83d6988ea4522e53d349c0cbff959bdabbbc16383646e2";
       var myHeadersClient = new Headers();
       myHeadersClient.append("X-Company-Login", "juegojuegos");
       myHeadersClient.append("X-Token", accessToken);
@@ -98,20 +94,20 @@ export default function App() {
   async function testTokens() {
     try {
       var reservationID = "6a63a2b0-a486-464f-96b5-703ce7ff0fae";
-      const details = {
-        user_id: reservationID,
-      };
+      const reservationData = await API.graphql(
+        graphqlOperation(
+          `query MyQuery {getReservation(id: "` +
+            reservationID +
+            `") { accessToken, refreshToken}}`
+        )
+      );
 
-      const reservationData = await API.graphql({
-        query: queries.getReservation,
-        variables: { input: details },
-      });
-
-      var accessToken = reservationData["accessToken"];
+      var accessTokenTest = reservationData.data.getReservation.accessToken;
+      var refreshTokenTest = reservationData.data.getReservation.refreshToken;
 
       var myHeaders = new Headers();
       myHeaders.append("X-Company-Login", "juegojuegos");
-      myHeaders.append("X-Token", accessToken);
+      myHeaders.append("X-Token", accessTokenTest);
 
       var requestOptions = {
         method: "GET",
@@ -123,50 +119,161 @@ export default function App() {
         "https://user-api-v2.simplybook.me/admin/bookings/1/links",
         requestOptions
       );
+
       var text = await response.text();
       var json_text = await JSON.parse(text);
+
+      console.log(json_text);
+
       if (json_text["code"] === "419") {
-        // return refreshOldToken(
-        //   "https://user-api-v2.simplybook.me/admin/auth/refresh-token",
-        //   refreshToken
-        // );
+        console.log("Correct");
+        var tokens = await refreshOldToken(refreshTokenTest);
+        const details = {
+          accessTokenTest: tokens[0],
+          refreshTokenTest: tokens[1],
+          id: "6a63a2b0-a486-464f-96b5-703ce7ff0fae",
+        };
+        console.log("Test: " + tokens[0]);
+        const reservationData = await API.graphql({
+          query: mutations.updateReservation,
+          variables: { input: details },
+        });
       }
-      // return [accessToken, refreshToken];
     } catch (err) {
       console.log({ err });
     }
   }
 
-  // async function refreshOldToken(refreshToken) {
-  //   var myHeaders = new Headers();
-  //   myHeaders.append("Content-Type", "application/json");
+  async function refreshOldToken(refreshToken) {
+    var myHeadersRefresh = new Headers();
+    myHeadersRefresh.append("Content-Type", "application/json");
 
-  //   var raw = JSON.stringify({
-  //     company: "juegojuegos",
-  //     refresh_token: refreshToken,
-  //   });
+    var rawRefresh = JSON.stringify({
+      company: "juegojuegos",
+      refresh_token: refreshToken,
+    });
 
-  //   var requestOptions = {
-  //     method: "POST",
-  //     headers: myHeaders,
-  //     body: raw,
-  //     redirect: "follow",
-  //   };
+    console.log(refreshToken);
 
-  //   const response = await fetch(
-  //     "https://user-api-v2.simplybook.me/admin/auth/refresh-token",
-  //     requestOptions
-  //   );
-  //   var text = await response.text();
-  //   var json_text = await JSON.parse(text);
-  //   return [json_text["token"], json_text["refresh_token"]];
-  // }
+    var requestOptionsRefresh = {
+      method: "POST",
+      headers: myHeadersRefresh,
+      body: rawRefresh,
+      redirect: "follow",
+    };
+
+    const responseRefresh = await fetch(
+      "https://user-api-v2.simplybook.me/admin/auth/refresh-token",
+      requestOptionsRefresh
+    );
+
+    var text = await responseRefresh.text();
+    var json_text = await JSON.parse(text);
+
+    if (json_text["code"] === "401") {
+      console.log("Correct");
+      var tokens = await makeToken();
+      const details = {
+        accessTokenTest: tokens[0],
+        refreshTokenTest: tokens[1],
+        id: "6a63a2b0-a486-464f-96b5-703ce7ff0fae",
+      };
+      console.log("Test: " + tokens[0]);
+      const reservationData = await API.graphql({
+        query: mutations.updateReservation,
+        variables: { input: details },
+      });
+    }
+
+    var textRefresh = await responseRefresh.text();
+    var json_textRefresh = await JSON.parse(textRefresh);
+    console.log(json_textRefresh);
+    return [json_textRefresh["token"], json_textRefresh["refresh_token"]];
+  }
+
+  async function makeToken() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      company: "juegojuegos",
+      login: "rishik@juego.juegos",
+      password: "4mvVvN9H8!!HMD@",
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    const token_response = await fetch(
+      "https://user-api-v2.simplybook.me/admin/auth?",
+      requestOptions
+    );
+    var text = await token_response.text();
+    var json_text = await JSON.parse(text);
+    return [json_text["token"], json_text["refresh_token"]];
+  }
+
+  async function makeBooking() {
+    /* starttime,
+    endtime,
+    providerID,
+    serviceID,
+    clientID,
+    accessToken */
+
+    var id = "6a63a2b0-a486-464f-96b5-703ce7ff0fae";
+
+    const reservationData = await API.graphql(
+      graphqlOperation(
+        `query MyQuery {getReservation(id: "` +
+          id +
+          `") { accessToken, reserveID }}`
+      )
+    );
+
+    var accessToken = reservationData.data.getReservation.accessToken;
+    var clientID = reservationData.data.getReservation.reserveID;
+
+    var myHeaders = new Headers();
+    myHeaders.append("X-Company-Login", "juegojuegos");
+    myHeaders.append("X-Token", accessToken);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      count: 1,
+      start_datetime: "2023-07-30 09:30:00",
+      end_datetime: "2023-07-30 09:45:00",
+      provider_id: "2",
+      service_id: "3",
+      client_id: clientID,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    const response = await fetch(
+      "https://user-api-v2.simplybook.me/admin/bookings",
+      requestOptions
+    );
+    var text = await response.text();
+    var json_text = await JSON.parse(text);
+    console.log("Booking ID: " + json_text["bookings"][0]["id"]);
+  }
 
   return (
     <div>
       <h1>Hello World!</h1>
       <Button title="Create New ReserveUser" onClick={createReservationUser} />
       <Button title="Refresh User Token" onClick={testTokens} />
+      <Button title="Make Booking" onClick={makeBooking} />
       {reservations.map((reservation) => (
         <div key={reservation.user_id}>
           <h3>{reservation.reserveID}</h3>
